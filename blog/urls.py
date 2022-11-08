ef8b7780f284dd35316b75268fe9be5d793f19bc
sevarasharopova <sevarasharopova23@gmail.com>
2022-11-08 08:39:57 +0500

    application

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from core.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('moshin1/', moshin1),
+    path('moshin2/', moshin2),
+    path('moshin3/', moshin3), 
+    path('moshin4/', moshin4), 
+    path('moshin5/', moshin5),
+    path('moshin6/', moshin6), 
+
+]
---
+++ core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from frontend import *
+
+def moshin1(request):
+    return render(request, 'BMW.html')
+
+def moshin2(request):
+    return render(request,'equinox.html')
+
+def moshin3(request):
+    return render(request,'kaptiva.html')
+
+def moshin4(request):
+    return render(request,'malibu.html')
+def moshin5(request):
+    return render(request,'orlando.html')
+
+def moshin6(request):
+    return render(request,'range rower.html')
